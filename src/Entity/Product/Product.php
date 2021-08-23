9527dc355e49c2e55f7b60b20425690502206c5e
Lookas (Łukasz) Konieczny <lookasziebice@gmail.com>
2021-08-23 22:03:33 +0200

    Changed property in product entity.
    Added nullable for true in color property.

--- src/Entity/Product/Product.php
+++ src/Entity/Product/Product.php
@@ -23,7 +23,7 @@ class Product extends BaseProduct
     /**
      * @var string
      *
-     * @ORM\Column(type="string")
+     * @ORM\Column(type="string", nullable=true)
      */
     private $color;
 
